9ef4898bc7c977b04f942f07685516e1b4bd0ef1
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-26 22:28:14 -0500

    Nominations can now have a nominated user and nominator user

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -1,14 +1,4 @@
 class Nomination < ApplicationRecord
-    # belongs_to :nominator, class_name: "Nomination", foreign_key: :nominator_id
-    # belongs_to :nominated, class_name: "Nomination", foreign_key: :nominated_id
-
-    # belongs_to :outbound_nomination, foreign_key: 'nominated_id', class_name: 'User' 
-    # belongs_to :inbound_nomination, foreign_key: 'nominator_id', class_name: 'User' 
-
-    # belongs_to :user
-    # belongs_to :nominator,class_name: "User", foreign_key: :nominator_id
-    # belongs_to :nominated,class_name: "User",foreign_key: :nominated_id
-
-    belongs_to :nominator, :class_name => 'User'
-    belongs_to :nominated, :class_name => 'User'
+    belongs_to :nominator, :class_name => 'User', optional: true
+    belongs_to :nominated, :class_name => 'User', optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,4 @@
 class User < ApplicationRecord
-
-    # has_many :nominations
-
-    # has_many :nomination_relationships, foreign_key: :nominated_id, class_name: 'Nomination'
-    # has_many :outbound_nominations, through: :nomination_relationships, source: :outbound_nomination
-    
-    # has_many :nominated_relationships, foreign_key: :nominator_id, class_name: 'Nomination'
-    # has_many :inbound_nominations, through: :nominated_relationships, source: :inbound_nomination
-
-    # has_many :nominations_out, class_name: "Nomination", foreign_key: 'nominator_id'
-    # has_many :nominations_in, class_name: "Nomination", foreign_key: 'nominated_id'
-
-    has_many :nomination_outs, :class_name => 'Nomination', :foreign_key => 'nominator_id'
-    has_many :nomination_ins, :class_name => 'Nomination', :foreign_key => 'nominated_id'
+    has_many :outbound_nominations, :class_name => 'Nomination', :foreign_key => 'nominator_id'
+    has_many :inbound_nominations, :class_name => 'Nomination', :foreign_key => 'nominated_id'
 end
