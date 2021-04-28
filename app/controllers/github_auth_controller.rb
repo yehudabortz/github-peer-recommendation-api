4cf0b86eec15fe9363a34b419e977d5549022fd5
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-28 10:48:51 -0500

    Update github_params to require 'github_auth'

--- app/controllers/github_auth_controller.rb
+++ app/controllers/github_auth_controller.rb
@@ -1,29 +1,31 @@
 class GithubAuthController < ApplicationController
+    skip_before_action :authorized
 
-  def github_callback      
-    redirect_uri = 'http://localhost:4000/auth/github_oauth2/callback'
-    response = HTTParty.get("https://github.com/login/oauth/access_token?client_id=#{ENV['GITHUB_CLIENT_ID']}&redirect_uri=#{redirect_uri}&client_secret=#{ENV['GITHUB_CLIENT_SECRET']}&code=#{github_params[:code]}")
-    token = response.split('access_token=')[1].split('&scope')[0]
-    create_user_from_github(token)
-  end
-
-  def create_user_from_github(token)
-    client = Octokit::Client.new(:access_token => token)
-    if User.find_by(github_username: client.user.login)
-        render :json { error: "Unable to login"}
-    else
-        user = User.create(github_username: client.user.login, avatar: client.user.avatar_url)
-        render :json {user}
+    def github_callback    
+      redirect_uri = 'http://localhost:3000/auth/github/callback'
+      response = HTTParty.get("https://github.com/login/oauth/access_token?client_id=#{ENV['GITHUB_CLIENT_ID']}&redirect_uri=#{redirect_uri}&client_secret=#{ENV['GITHUB_CLIENT_SECRET']}&code=#{github_params[:code]}")
+      token = response.split('access_token=')[1].split('&scope')[0]
+      create_user_from_github(token)
     end
-    # binding.pry
-  end
+
+    def create_user_from_github(token)
+      client = Octokit::Client.new(:access_token => token)
+      user = User.find_by(github_username: client.user.login)
+      if user
+            render json: {user: user}
+          else
+            user = User.create(github_username: client.user.login, avatar: client.user.avatar_url)
+            render json: {user: user}
+      end
+    end
+
+    private
+    
+    def github_params
+        params.require(:github_auth).permit(:code)
+    end
 end
 
-private
-
-def github_params
-    params.permit(:code)
-end
 
 # THIS LINK IN THE BROWSER DOWNLOADS THE USER TOKEN
 # https://github.com/login/oauth/access_token?client_id=#{ENV['GITHUB_CLIENT_ID']}&redirect_uri=http://localhost:4000/auth/github_oauth2/callback&client_secret=#{ENV['GITHUB_CLIENT_SECRET']}&code=#{params[:code]}
