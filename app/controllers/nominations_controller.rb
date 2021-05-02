c39335f4cb079510edd309088ebc0d4ec8cfcc64
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-02 11:29:43 -0500

    Nomintions now able to be created and assocaited

--- app/controllers/nominations_controller.rb
+++ app/controllers/nominations_controller.rb
@@ -1,6 +1,19 @@
 class NominationsController < ApplicationController
 
     def create
+        nominated = User.where(github_username: nomination_params[:github_username]).first_or_create do |nominated|
+            nominated.github_username = nomination_params[:github_username]
+            nominated.avatar = nomination_params[:avatar]
+        end
+        nomination = Nomination.new()
+        nomination.nominator = current_user
+        nomination.nominated = nominated
+        if  !current_user.outbound_nominations.where(nominated_id: nominated.id).empty?
+            render json: {message: "Unable to nominate user"}
+        else
+            nomination.save
+            render json: {user: nominated}
+        end
     end
     
     def show
@@ -9,4 +22,9 @@ class NominationsController < ApplicationController
     def update
     end
 
+    private
+
+    def nomination_params 
+        params.require(:nomination).permit(:github_username, :avatar)
+    end
 end
