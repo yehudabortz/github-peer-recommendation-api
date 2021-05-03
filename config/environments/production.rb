972f3f2e6e4beabcfcfd9f3efac9c4597d91339e
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-02 20:15:18 -0500

    Sendgrid config for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,14 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+    config.action_mailer.delivery_method = :sendgrid_actionmailer
+  config.action_mailer.sendgrid_actionmailer_settings = {
+  api_key: ENV['SENDGRID_API_KEY'],
+  raise_delivery_errors: true
+}
+
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
