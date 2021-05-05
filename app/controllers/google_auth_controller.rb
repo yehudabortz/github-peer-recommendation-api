c8e421757ed4f7fe750d369f152225828b6cb655
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-05 14:50:31 -0500

    Sign in with google

---
+++ app/controllers/google_auth_controller.rb
@@ -0,0 +1,28 @@
+class GoogleAuthController < ApplicationController
+    skip_before_action :authorized
+
+    def google_callback   
+        url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{google_params[:id_token]}"                  
+        response = HTTParty.get(url)      
+        create_user_from_google(response)
+    end
+    
+    def create_user_from_google(response)
+        if response["aud"] == ENV['GOOGLE_CLIENT_ID']
+            user = User.where(email: response["email"], provider: "google").first_or_create do |user|
+                user.name = response["name"]
+                user.email = response["email"]
+                user.avatar = response["picture"]
+                user.jwt_token = encode_token({user_id: user.id})
+            end
+            render json: {user: user}
+        else
+            render json: {message: "Unable to login"}
+        end
+    end
+
+    private
+    def google_params
+        params.require(:google_auth).permit(:access_token, :id_token)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/users/:id/account', to: 'users#current_user_account'
   post '/auth/github_oauth2/callback', to: 'github_auth#github_callback'
+  post '/auth/google_oauth2/callback', to: 'google_auth#google_callback'
 
   get '/search/github/users', to: 'github_search#search_users'
 
