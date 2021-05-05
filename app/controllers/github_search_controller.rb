ab3c552e1918d34b17c86d6872f933a57bfcf39e
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-05 15:55:52 -0500

    Add auth to headers

--- app/controllers/github_search_controller.rb
+++ app/controllers/github_search_controller.rb
@@ -6,7 +6,12 @@ class GithubSearchController < ApplicationController
         # binding.pry
         # response = HTTParty.get("https://api.github.com/users/#{search_params[:q]}")
         # response = HTTParty.get("https://api.github.com/users/#{search_params[:q]}?client_id=#{ENV['GITHUB_CLIENT_ID']}&client_secret=#{ENV['GITHUB_CLIENT_SECRET']}")
-        response = HTTParty.get("https://api.github.com/users/#{search_params[:q]}?access_token=#{ENV['GITHUB_ACCESS_TOKEN']}")
+        # response = HTTParty.get("https://api.github.com/users/#{search_params[:q]}?access_token=#{ENV['GITHUB_ACCESS_TOKEN']}")
+        response = HTTParty.get("https://api.github.com/users/#{search_params[:q]}", 
+            headers: {
+                access_token: ENV['GITHUB_ACCESS_TOKEN']
+            }
+        )
         # users = response["items"][0..10]
         render :json => { user: response }
         # render :json => { users: users }
