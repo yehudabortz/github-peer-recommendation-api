3e13d59282b3a6b605e45da909234d3feb052320
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-06 14:35:47 -0500

    Score only reflects active nominations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 def current_user_account
-  render json: {user: current_user, nominated_users: current_user.find_nominated_users, score: current_user.inbound_nominations.count}, status: :created
+  render json: {user: current_user, nominated_users: current_user.find_nominated_users, score: current_user.inbound_nominations.where(active: true).count}, status: :created
 end
 
 
