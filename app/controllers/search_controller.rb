d19c2d0f24327de03a51e407f48d2071d32637c3
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-11 22:26:44 -0500

    Users now filterable by most nominations and open to work

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,24 +1,45 @@
 class SearchController < ApplicationController
   def search_index
     if admin?
+    users = []
       page = search_params[:page].to_i
       display_count = search_params[:display_count]
       offset = page * display_count.to_i
       results_count = User.all.count
 
-      if search_params[:filter] #&& search_params[:condition]
-        binding.pry
-
-        
+      if search_params[:filter].values.any?  #&& search_params[:condition]
         # ITERATE THROUGH AND QUERY BASED ON EACH KEY VALUE
-        filter_keys = search_params[:filter].keys
-        filter_keys.map do |key|
-            puts "#{key} #{search_params[:filter].values_at(key)}"
-             search_params[:filter].values_at(key)
+        filters = search_params[:filter].compact
+        filter_keys = filters.keys
+        filter_keys.each do |key|
+            value = search_params[:filter].values_at(key)[0]
+            case filter_keys.count
+            when 1
+                case key
+                when "open_to_work"
+                    results_count = User.where("#{key}" => "#{value}").offset(offset).limit(display_count).count
+                    users = User.where("#{key}" => "#{value}").offset(offset).limit(display_count).reverse_order
+                    break
+                when "inbound_nominations"
+                    results_count = User.find_most_inbound_nominations.count.count
+                    users = User.find_most_inbound_nominations.offset(offset).limit(display_count)
+                    break
+                    # binding.pry
+                end
+            when 2
+                case key
+                when "open_to_work" || "inbound_nominations"
+                    results_count =  User.find_most_inbound_nominations.where("#{key}" => "#{value}").offset(offset).limit(display_count).count.count
+                    users = User.find_most_inbound_nominations.where("#{key}" => "#{value}").offset(offset).limit(display_count)
+                    break
+                end
+            end
         end
-
-        results_count = User.where("#{search_params[:filter]}": "#{search_params[:condition]}").count
-        users = User.where("#{search_params[:filter]}": "#{search_params[:condition]}").offset(offset).limit(display_count).reverse_order
+        # users = User.where(open_to_work: true).offset(offset).limit(display_count).reverse_order
+        # Order name ABC
+        # User.order('name ASC')
+        # results_count = User.where("#{search_params[:filter]}": "#{search_params[:condition]}").count
+        # users = User.where("#{search_params[:filter]}": "#{search_params[:condition]}").offset(offset).limit(display_count).reverse_order
       else
         users = User.offset(offset).limit(display_count).reverse_order
       end
@@ -42,3 +63,10 @@ class SearchController < ApplicationController
   end
 
 end
+
+
+# FINDS USERS WHERE THEIR OUTBOUND NOMINATIONS ARE ACCEPTED
+# User.joins(:outbound_nominations).where(:outbound_nominations => { :accepted => true })
+
+# FINDS NOMINATIONS WHERE THE NAME IS X
+# Nomination.joins(:nominator).where(:nominator => {name: "Example name"})
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
     
     # validates :github_id, uniqueness: { case_sensitive: false }
 
+
+    def self.find_most_inbound_nominations
+        User.joins(:inbound_nominations).group("users.id").order("count(nominated_id) DESC")
+    end
+
     def find_nominated_users
         Nomination.all.where(nominator_id: self.id, active: true).map do |nomination|
             nomination.nominated
