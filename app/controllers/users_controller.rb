e78836d54b9e0281329e14ece8e6d1ebc83625a5
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-12 11:53:12 -0500

    Remove un used params acceptance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,17 @@ class UsersController < ApplicationController
   end
       
   def current_user_account
-    render json: {user: current_user, nominated_users: current_user.find_nominated_users, score: current_user.inbound_nominations.count}, status: :created
+    render json: {user: current_user, nominated_users: current_user.find_nominated_users,
+      co_worker_nominated_users: current_user.find_co_worker_nominated_users,
+      past_co_worker_nominated_users: current_user.find_past_co_worker_nominated_users,
+      score: current_user.inbound_nominations.count}, status: :created
   end
 
 
   private
 
   def user_params
-      params.permit(:id, :password, :page, :filter, :display_count)
+      params.permit(:id)
   end
 
 
