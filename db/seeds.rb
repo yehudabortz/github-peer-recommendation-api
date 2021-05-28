c9278dbe0e2db1d01b317471719f6bb3b9d1dce4
Yehuda Bortz <yehudabortz@icloud.com>
2021-05-27 19:19:40 -0500

    remove open to work

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
     end
 
 
-    first_user = User.create(linkedin_handle: Faker::Internet.username(specifier: 5..8), name: Faker::Name.unique.name, email: Faker::Internet.email, open_to_work: open_to_work)
+    first_user = User.create(linkedin_handle: Faker::Internet.username(specifier: 5..8), name: Faker::Name.unique.name, email: Faker::Internet.email)
     first_user.work_preference.update(open_to_targeted_jobs: true, willing_to_relocate: false)
-    second_user = User.create(linkedin_handle: Faker::Internet.username(specifier: 5..8), name: Faker::Name.unique.name, email: Faker::Internet.email, open_to_work: open_to_work)
+    second_user = User.create(linkedin_handle: Faker::Internet.username(specifier: 5..8), name: Faker::Name.unique.name, email: Faker::Internet.email)
     second_user.work_preference.update(open_to_targeted_jobs: true, willing_to_relocate: false)
 
     6.times do
